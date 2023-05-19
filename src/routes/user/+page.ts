6cbd84ae195e73ac442b7313cb56e772735152a3
SOHAMPATEL1483 <86504280+SOHAMPATEL1483@users.noreply.github.com>
2023-05-19 22:49:59 +0530

    solved bug

--- src/routes/user/+page.ts
+++ src/routes/user/+page.ts
@@ -12,8 +12,10 @@ export const load: PageLoad = async ({ fetch }) =>
         credentials: 'include',
     })
     let provider_order = await res2.json();
+    console.log(provider_order);
     //@ts-ignore
     user_order.orders.sort((a, b) => a.updatedAt > b.updatedAt ? -1 : 1)
-    provider_order.sort((a: any, b: any) => a.updatedAt > b.updatedAt ? -1 : 1)
+    if (!provider_order.msg)
+        provider_order.sort((a: any, b: any) => a.updatedAt > b.updatedAt ? -1 : 1)
     return { user_order, provider_order };
 }
--- src/routes/user/changepass/+page.server.ts
+++ src/routes/user/changepass/+page.server.ts
@@ -1,6 +1,14 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import API_URL from '../../../stores/store';
 import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ cookies }) =>
+{
+    if (!cookies.get('sessionId'))
+        throw error(404, {
+            message: 'You must be logged in to view this page'
+        });
+};
 
 export const actions: Actions = {
     changePassword: async ({ request, fetch }) =>
