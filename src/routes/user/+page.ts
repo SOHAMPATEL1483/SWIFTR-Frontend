ec32baab6ac6847905b8706e612d3a9b5e64fccc
SOHAMPATEL1483 <86504280+SOHAMPATEL1483@users.noreply.github.com>
2023-05-19 23:44:57 +0530

    solved error on loading page

--- src/routes/user/+page.ts
+++ src/routes/user/+page.ts
@@ -1,21 +1,22 @@
 import type { PageLoad } from './$types';
 import API_URL from '../../stores/store';
-export const load: PageLoad = async ({ fetch }) =>
+export const load: PageLoad = async ({ fetch, parent }) =>
 {
+    let { user } = await parent();
     let res: Response = await fetch(`${API_URL}/api/v1/stripe/getOrders`, {
         method: 'GET',
         credentials: 'include',
     })
-    let user_order = await res.json();
-    let res2: Response = await fetch(`${API_URL}/api/v1/stripe/getProviderOrders`, {
-        method: 'GET',
-        credentials: 'include',
-    })
-    let provider_order = await res2.json();
-    console.log(provider_order);
-    //@ts-ignore
-    user_order.orders.sort((a, b) => a.updatedAt > b.updatedAt ? -1 : 1)
-    if (!provider_order.msg)
-        provider_order.sort((a: any, b: any) => a.updatedAt > b.updatedAt ? -1 : 1)
+    let user_order = await res.json(), provider_order = [];
+    if (user.roles === "provider")
+    {
+        let res2: Response = await fetch(`${API_URL}/api/v1/stripe/getProviderOrders`, {
+            method: 'GET',
+            credentials: 'include',
+        })
+        provider_order = await res2.json();
+    }
+    user_order.orders.sort((a: any, b: any) => a.updatedAt > b.updatedAt ? -1 : 1);
+    provider_order.sort((a: any, b: any) => a.updatedAt > b.updatedAt ? -1 : 1);
     return { user_order, provider_order };
 }
